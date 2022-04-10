cb945df459fa75f0ef58496dc2ce0180312e2219
kyoko-i <iida@linkedge.jp>
2022-04-10 17:29:19 +0900

    Initial commit

---
+++ test/cdk_vpn_bastion.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkVpnBastion from '../lib/cdk_vpn_bastion-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_vpn_bastion-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkVpnBastion.CdkVpnBastionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
